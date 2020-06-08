e5783a919290c42fbdf2d3dde81560b281995c32
Mike Williams <mdub@dogbiscuit.org>
2020-06-08 21:07:23 +1000

    Implement costFunction for logistic regression.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,12 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+predictions = sigmoid(X * theta);
+J = sum(-y .* log(predictions) - (1 - y) .* log(1 - predictions)) / m;
+grad = sum((predictions - y) .* X)' / m;
 
 % =============================================================
 
